a875e72b4f6bd10c9c98eb74487ac406881f3c14
devrayhankabir <devrayhankabir@gmail.com>
2021-07-16 15:05:36 +0600

    class and object

--- index.php
+++ index.php
@@ -1,4 +1,51 @@
+<h1>Hello world</h1><hr>
 <?php 
+
+class Hello{
+
+    public function sayHello(){
+
+        echo "hello world";
+    }
+}
+
+$ob = new Hello;
+
+$ob->sayHello();
+
+?>
+
+<h1>Object and classes</h1> <hr>
+<?php 
+class User{
+
+    public $id;
+    public $name;
+    public $email;
+    public $phone;
+    
+
+    public function setData($i, $n, $e, $p){
+
+        $this->id = $i;
+        $this->name = $n;
+        $this->email = $e;
+        $this->phone = $p;
+
+    }
+
+    public getData(){
+
+        return "User id is: ".$this->id;
+        return "User name is: ".$this->name;
+        return "User email is: ".$this->email;
+        return "User phone is: ".$this->phone;
+    }
+}
+
+$object = new User;
+$object->setData(9999, "rayhan kabir", "devrayhankabir@gmail.com", 01643163478);
+echo $object->getData();
 
 
 ?>
