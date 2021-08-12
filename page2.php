3dd166ae50b21a54721fcadfa788ab5d24702388
devrayhankabir <devrayhankabir@gmail.com>
2021-08-13 01:45:10 +0600

    abstract inheritance

--- page2.php
+++ page2.php
@@ -41,3 +41,71 @@ $obj->myData();
 
 
 ?>
+
+<h1>Interface</h1>
+<br>
+
+<?php 
+interface Test{
+    const A = 100;
+
+    public function showValue(); //by default it is abstract method, because interface doesn't allow normal method...
+}
+
+interface Test2{
+    const B = 200;
+    public function showValue();
+}
+
+class Main implements Test,Test2{
+
+    public function showValue(){
+        echo Test::A.'<br>';
+        echo Test2::B;
+    }
+}
+
+$ob = new Main();
+$ob->showValue();
+
+
+?>
+
+<h2>Interface inheritance</h2>
+<br>
+
+<?php
+
+interface Test3{
+
+    const A = 300;
+    public function showNumber();
+}
+interface Test4 extends Test3{
+
+    const B = 200;
+}
+
+class Info{
+    private $name;
+
+    public function showInfo($n){
+        
+        echo "Name is: ".$this->name = $n;
+    }
+}
+
+class User2 extends Info implements Test4{
+
+    public function showNumber()
+    {
+        echo Test3::A.'<br>';
+        echo Test4::B;
+    }
+}
+
+$ob = new User2;
+$ob->showNumber();
+$ob->showInfo();
+
+?>
