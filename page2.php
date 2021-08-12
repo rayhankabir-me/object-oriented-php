686c9ec9bb23c8af0b96bddae90329e56ac47dd9
devrayhankabir <devrayhankabir@gmail.com>
2021-08-12 22:38:18 +0600

    abstract class initiated

--- page2.php
+++ page2.php
@@ -1,7 +1,43 @@
 <h1>Abstract class</h1>
 <hr>
 
-<?php 
+<?php
+
+use Person as GlobalPerson;
+use User as GlobalUser;
+
+abstract class Person{
+
+    public $name;
+    public $age;
+
+    public function __construct($n, $a)
+    {
+        $this->name = $n;
+        $this->age = $a;
+    }
+
+    public function showData(){
+
+        echo "I am from abstract class";
+    }
+
+    abstract public function myData();
+
+    
+}
+
+class User extends Person{
+
+    public function myData(){
+
+        echo "my name is ".$this->name." and my age is ".$this->age;
+    }
+}
+
+
+$obj = new User("rayhan", 24);
+$obj->myData();
 
 
 
