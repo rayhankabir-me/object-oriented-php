b64a351a0bdd40755b6d94993919d0300b20129c
devrayhankabir <devrayhankabir@gmail.com>
2021-08-15 02:41:22 +0600

    set method added

---
+++ magic_methods/set.php
@@ -0,0 +1,35 @@
+<?php
+
+//set values in private properties from outside of the class
+
+use User as GlobalUser;
+
+class User{
+
+    private $name;
+    private $age;
+
+    public function __set($Propertyname, $Propertyvalue)
+    {
+        if($Propertyname == "name"){
+
+            $this->name = $Propertyvalue;
+        }
+        elseif($Propertyname == "age"){
+            $this->age = $Propertyvalue;
+        }
+    }
+
+    public function showValue(){
+
+        echo "name is: ".$this->name . "<br>";
+        echo "age is ".$this->age . "<br>";
+    }
+}
+
+$ob = new User;
+$ob->name = "rayhan";
+$ob->age = 24;
+$ob->showValue();
+
+?>
