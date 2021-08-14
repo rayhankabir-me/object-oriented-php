f613df94141ee87b4c4d2fc0f471de96a473ac4a
devrayhankabir <devrayhankabir@gmail.com>
2021-08-15 00:20:13 +0600

    function called

--- polymorphism.php
+++ polymorphism.php
@@ -1,4 +1,48 @@
+<h1>Polymorphism</h1>
+<br>
 <?php
 
+interface Shape{
+
+    public function calculateArea();
+}
+
+class Circle implements Shape{
+
+    private $radius;
+
+    public function __construct($r) 
+    {
+        $this->radius = $r;
+    }
+
+    public function calculateArea()
+    {
+        echo "The area of circle is: ".pi().$this->radius*$this->radius."<br>";
+    }
+}
+
+class Rectangle implements Shape{
+
+    private $width;
+    private $height;
+
+    public function __construct($w, $h)
+    {
+        $this->width = $w;
+        $this->height = $h;
+    }
+
+    public function calculateArea()
+    {
+        echo "The area of rectangle is: ".$this->width*$this->height."<br>";
+    }
+}
+
+$ob = new Circle(2);
+$ob->calculateArea();
+
+$object = new Rectangle(5, 6);
+$object->calculateArea();
 
 ?>
